c54e2e6c959296671aa8ed4da334191e6adc4fc2
camel_assault <camels.personal@gmail.com>
2025-05-28 15:22:04 -0700

    Initial main and requirements files committed

---
+++ main.py
@@ -0,0 +1,14 @@
+# this allows us to use code from
+# the open-source pygame library
+# throughout this file
+import pygame
+
+# Defining main function
+def main():
+    print("Starting Asteroids!")
+
+
+# Using the special variable 
+# __name__
+if __name__=="__main__":
+    main()
