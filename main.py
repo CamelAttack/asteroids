831b79145b5320025dc746f3fd86652f7996efad
camel_assault <camels.personal@gmail.com>
2025-05-29 14:58:29 -0700

    Created drawable and updatable sprite groups, added player object to both groups four updating/drawing

--- main.py
+++ main.py
@@ -13,6 +13,10 @@ def main():
     clock = pygame.time.Clock()
     dt = 0
 
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+    Player.containers = (updatable, drawable)
+
     center_x = int(SCREEN_WIDTH / 2)
     center_y = int(SCREEN_HEIGHT / 2)
     player_object = Player(center_x, center_y)
@@ -22,8 +26,9 @@ def main():
             if event.type == pygame.QUIT:
                 return
         screen.fill("black")
-        player_object.update(dt)
-        player_object.draw(screen)
+        updatable.update(dt)
+        for some_object in drawable:
+            some_object.draw(screen)
         pygame.display.flip()
         dt = clock.tick(60) /1000
     
