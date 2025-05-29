8a206659561d0fc491b9bf90cf352514b5c3e808
camel_assault <camels.personal@gmail.com>
2025-05-29 14:51:21 -0700

    Added wasd movement to player object

--- main.py
+++ main.py
@@ -12,7 +12,7 @@ def main():
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     clock = pygame.time.Clock()
     dt = 0
-    
+
     center_x = int(SCREEN_WIDTH / 2)
     center_y = int(SCREEN_HEIGHT / 2)
     player_object = Player(center_x, center_y)
@@ -22,6 +22,7 @@ def main():
             if event.type == pygame.QUIT:
                 return
         screen.fill("black")
+        player_object.update(dt)
         player_object.draw(screen)
         pygame.display.flip()
         dt = clock.tick(60) /1000
