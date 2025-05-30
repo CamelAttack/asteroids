249ba0e17edd1c37b4cd0a72213c85e8fd01edb3
camel_assault <camels.personal@gmail.com>
2025-05-30 16:45:50 -0700

    Added shot class and shoot functionallity to player, no functionallity for shot jsut graphical.

--- main.py
+++ main.py
@@ -6,6 +6,7 @@ import sys
 import circleshape
 from asteroid import Asteroid
 from player import Player
+from shot import Shot
 from asteroidfield import AsteroidField
 from constants import *
 
@@ -19,10 +20,12 @@ def main():
     updatable = pygame.sprite.Group()
     drawable = pygame.sprite.Group()
     asteroids = pygame.sprite.Group()
+    shots = pygame.sprite.Group()
+    
     Player.containers = (updatable, drawable)
     Asteroid.containers = (asteroids, updatable, drawable)
     AsteroidField.containers = (updatable)
-    
+    Shot.containers = (shots, updatable, drawable)
 
     center_x = int(SCREEN_WIDTH / 2)
     center_y = int(SCREEN_HEIGHT / 2)
